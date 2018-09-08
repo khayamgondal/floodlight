60fedbc39d78e21381a4e923c90c8de9e11dc2fb
Khayam Gondal <khayam.gondal@gmail.com>
2018-09-08 14:41:10 -0400

    implemented conn termintation. not tested yet

--- src/main/java/net/floodlightcontroller/sos/web/ConnectionResource.java
+++ src/main/java/net/floodlightcontroller/sos/web/ConnectionResource.java
@@ -4,6 +4,7 @@ import net.floodlightcontroller.core.IListener;
 import net.floodlightcontroller.packet.Data;
 import net.floodlightcontroller.sos.*;
 import org.restlet.resource.Delete;
+import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +13,7 @@ public class ConnectionResource extends ServerResource {
     protected static Logger log = LoggerFactory.getLogger(ServerResource.class);
 
 
-    @Delete
+    @Post
     public void deleteConnection(String json) {
         ISOSService sosService = (ISOSService) getContext().getAttributes().get(ISOSService.class.getCanonicalName());
         ISOSTerminationStats stats = SOSTerminationStats.parseFromJson(json);
@@ -22,7 +23,6 @@ public class ConnectionResource extends ServerResource {
             log.error("Could not locate UUID {} in connection storage. Report SOS bug", stats.getTransferID());
         }
         if (conn.getClientSideAgent().getActiveTransfers().contains(stats.getTransferID()) &&
-                conn.getClientSideAgent().getIPAddr().equals(stats.getSourceAddress()) &&
                 stats.isClientSideAgent()) {
             log.warn("Received termination message from client side agent {} for transfer ID {}", conn.getClientSideAgent().getIPAddr(), stats.getTransferID());
             conn.getClientSideAgent().removeTransferId(stats.getTransferID());
@@ -32,7 +32,6 @@ public class ConnectionResource extends ServerResource {
             }
             /* continue; we might have just removed the 2nd agent */
         } else if (conn.getServerSideAgent().getActiveTransfers().contains(stats.getTransferID()) &&
-                conn.getServerSideAgent().getIPAddr().equals(l3.getSourceAddress()) &&
                 !stats.isClientSideAgent()) {
             log.warn("Received termination message from server side agent {} for transfer ID {}", conn.getServerSideAgent().getIPAddr(), stats.getTransferID());
             conn.getServerSideAgent().removeTransferId(stats.getTransferID());
